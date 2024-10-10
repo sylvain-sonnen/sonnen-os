42b6ef0ac61b7f94ae0c0364e86db3f31be3cb3a
sylvain-sonnen <s.gaffie@sonnen.de>
2024-10-10 15:30:53 +0200

    remove wezterm (nightly)

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ curl -Lo /tmp/rpm-repos/rpmfusion-free-release-"${RELEASE}".noarch.rpm "${RPMFUS
 curl -Lo /tmp/rpm-repos/rpmfusion-nonfree-release-"${RELEASE}".noarch.rpm "${RPMFUSION_MIRROR_RPMS}"/nonfree/fedora/rpmfusion-nonfree-release-"${RELEASE}".noarch.rpm
 
 mkdir -p /tmp/rpms
-curl -Lo /tmp/rpms/wezterm-nightly-fedora40.rpm https://github.com/wez/wezterm/releases/download/nightly/wezterm-nightly-fedora40.rpm
+# curl -Lo /tmp/rpms/wezterm-nightly-fedora40.rpm https://github.com/wez/wezterm/releases/download/nightly/wezterm-nightly-fedora40.rpm
 
 # curl -Lo /etc/yum.repos.d/_copr_ryanabx-cosmic.repo \
 #   https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-"${RELEASE}"/ryanabx-cosmic-epoch-fedora-"${RELEASE}".repo
